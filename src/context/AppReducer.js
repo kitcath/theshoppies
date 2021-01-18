79fc943bff2d2dd7d357690ef66ec5e40cd5e8b4
kitcath <catherine.vangheluwe@mail.mcgill.ca>
2021-01-17 21:53:11 -0500

    forgot comma

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -33,8 +33,7 @@ export default (state, action) => {
                     nominationList: [...state.nominationList]
                 };
             }else if (state.nominationList.length === 4){
-                alert("You reached the maximum number of nominees! Please view your selection in the NOMINATION LIST tab. ")
-        
+                alert("You reached the maximum number of nominees! Please view your selection in the NOMINATION LIST tab. ");
                 return {
                     ...state,
                     nominationList:[action.payload, ...state.nominationList],
